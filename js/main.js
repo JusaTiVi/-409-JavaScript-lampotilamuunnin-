8c48e91878fc294151b98bc4269a0c70b891bb1f
JusaTiVi <jusa.itkonen@esedulainen.fi>
2026-04-09 11:33:17 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
 var valinta = document.getElementById("asetus");
+const selected = document.querySelector('input[name="desimaali"]:checked');
+function muunna() {
+    document.getElementById("result").textContent = selected.value;
+}
