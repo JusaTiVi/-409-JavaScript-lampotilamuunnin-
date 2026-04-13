f7f41d73e3405dffaed8dcfef910ff365af9fbc6
JusaTiVi <jusa.itkonen@esedulainen.fi>
2026-04-13 13:02:52 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
-var valinta = document.getElementById("asetus");
-const selected = document.querySelector('input[name="desimaali"]:checked');
+
 function muunna() {
-    document.getElementById("result").textContent = selected.value;
+    let luku = parseFloat(document.getElementById("luku").value);
+
+    let asetus = document.getElementById("asetus").value;
+
+    let desimaali = parseInt(document.querySelector('input[name="desimaali"]:checked').value);
+
+    if (asetus === "celsius") {
+        tulos = (luku * 1.8) + 32
+    }
+    else if (asetus === "fahrenheit") {
+        tulos = (luku - 32) / 1.8
+    }
+    tulos = parseFloat(tulos.toFixed(desimaali));
+
+    document.getElementById("result").textContent =
+    "Tulos: " + tulos;
 }
