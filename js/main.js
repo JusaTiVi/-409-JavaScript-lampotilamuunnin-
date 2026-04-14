7e99922d6e077bf23c71398203a115a27ae5ef40
JusaTiVi <jusa.itkonen@esedulainen.fi>
2026-04-14 12:06:41 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,18 +2,35 @@
 function muunna() {
     let luku = parseFloat(document.getElementById("luku").value);
 
+    if (isNaN(luku)) {
+        document.getElementById("result").textContent =
+        "virhe, syötä vain numero";
+        return;
+    }
+
     let asetus = document.getElementById("asetus").value;
 
     let desimaali = parseInt(document.querySelector('input[name="desimaali"]:checked').value);
 
     if (asetus === "celsius") {
         tulos = (luku * 1.8) + 32
+        vertaus = luku
     }
     else if (asetus === "fahrenheit") {
         tulos = (luku - 32) / 1.8
+        vertaus = tulos
     }
-    tulos = parseFloat(tulos.toFixed(desimaali));
+    tulos = tulos.toFixed(desimaali);
 
     document.getElementById("result").textContent =
     "Tulos: " + tulos;
+
+    if (vertaus < -273.15) {
+        document.getElementById("nollapiste").textContent =
+        "kylmempi kuin absoluuttinen nollapiste"
+    }
+    else if (vertaus > -273.15) {
+        document.getElementById("nollapiste").textContent =
+        ""
+    }
 }
